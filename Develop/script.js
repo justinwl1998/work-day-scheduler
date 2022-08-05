bdc759eb31a464ee849a54c2a47f345503098992
Justin Liu <justinwl2097@gmail.com>
2022-08-05 17:59:30 -0500

    added comments to script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,14 @@
-console.log("Hello mo, goodbye mo.");
-
 var curDate = moment().format("dddd, MMMM Do");
 var curHour = moment().format("HH");
 var timeBlocks = $(".timeBlock");
 var eventTexts = $("textarea");
+// In the 0th index, the current date is there, to facillitate resetting the entire schedule if the day passes
 var schedule = [curDate, "", "", "", "", "", "", "", "", ""];
 
 var dateUpdater = setInterval(function() {
 
     // update the date if the time ever passes
     if (moment().format("dddd, MMMM Do") !== curDate) {
-        console.log("The date has changed!")
         curDate = moment().format("dddd, MMMM Do")
         $("#currentDay").text(curDate);
 
@@ -21,8 +19,6 @@ var dateUpdater = setInterval(function() {
         for (var i = 0; timeBlocks.length; i++) {
             eventTexts.eq(i).val("");
         }
-
-        //$("textarea").eq(i).parent().parent() <-- this is dumb, why
 
         // and clear the internal schedule array
         schedule = [curDate, "", "", "", "", "", "", "", "", ""];
@@ -42,6 +38,7 @@ var dateUpdater = setInterval(function() {
 
 function updateTimeblockStatus() {
     for (var i = 0; i < timeBlocks.length; i++) {
+        // Set timeblock status based on it if is before, during or after an hour
         if (moment().isBefore(moment({ hour:9+i, minute: 0 }))) {
             timeBlocks.eq(i).children().eq(1).addClass("future");
         }
@@ -57,20 +54,15 @@ function updateTimeblockStatus() {
 function init() {
     // initialize local storage if it hasn't been already
     if (localStorage.getItem('dateStorage') === null) {
-        console.log("No local storage defined!")
         localStorage.setItem('dateStorage', JSON.stringify(schedule));
     }
     else {
-        console.log("Local storage is defined");
-
         // if it's defined, just retrieve the stored events in localStorage
         schedule = JSON.parse(localStorage.getItem('dateStorage'));
         // and then put them in each textarea
 
         for (var i = 1; i < schedule.length; i++) {
-            console.log("Replacing schedule events...");
             console.log(eventTexts.eq(i-1));
-            console.log("schedule index " + i + ": " +schedule[i]);
             eventTexts.eq(i-1).val(schedule[i]);
         }
     }
@@ -83,16 +75,15 @@ function init() {
 }
 
 $(".saveBtn").on("click", function() {
-    console.log($(this))
-    console.log($(this).parent().attr('class').split(/\s+/));
-    console.log("The button clicked is for hour: " + $(this).parent().attr('class').split(/\s+/)[0])
-    console.log("Index: " + $(this).parent().index());
-    console.log
+    // get index the button belongs to and event text from textarea
     var index = $(this).parent().index();
-    var eventText = $("textarea").eq(index).val();
+    var eventText = eventTexts.eq(index).val();
 
+    // Put the event in the schedule array, with the index offset by 1
     schedule[index+1] = eventText;
 
+
+    // Update localStorage
     localStorage.setItem('dateStorage', JSON.stringify(schedule));
 
 });
